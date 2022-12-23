93f5113fa97f0e08376e562f3356c50ee550ff5c
MaRi <119165826+MariTurmanidze@users.noreply.github.com>
2022-12-23 18:28:49 +0400

    end

---
+++ script.js
@@ -0,0 +1,9 @@
+ // Get data
+ const nameinput = document.querySelector("#name");
+ const email = Document.querySelector("#Email");
+ const message = document.querySelector("#message");
+ const success = document.querySelector("success");
+ const errorNodes = document.querySelector(".error");
+
+ //vali data
+ function validataForm()
